20d97d1df34be7872b2bd2bada830c6b3e4088df
Yogi Prakoso <yogiarrazy@gmail.com>
2024-03-24 15:08:51 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+          port:'',
+          pathname: '/**',
+        },
+      ]
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+  }
 
 export default nextConfig;
